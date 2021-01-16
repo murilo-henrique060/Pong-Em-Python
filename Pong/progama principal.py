914ef61dd14d7a672e174fa4ea106add2d441818
Murilo Henrique <77072462+murilo-henrique060@users.noreply.github.com>
2021-01-16 15:39:22 -0300

    ainda não consequi corrigir o bug de quando a bola acerta o canto do jogador

--- Pong/progama principal.py
+++ Pong/progama principal.py
@@ -1,82 +1,131 @@
-import pygame
+import pygame, time
 pygame.init()
-LARGURA_JANELA = 600
-ALTURA_JANELA = 500
+
+#Bola
 TAMANHO_BOLA = 30
-LARGURA_JOGADOR = 20
+VELOCIDADEX = 8
+VELOCIDADEY = 4
+
+#JANELA
+LARGURA_JANELA = 700
+ALTURA_JANELA = 600
+
+#Jogador
+LARGURA_JOGADOR = 30
 ALTURA_JOGADOR = 150
-VELOCIDADE_JOGADOR = 7
-VELOCIDADE_BOLAX = 7
-VELOCIDADE_BOLAY = 4
+VELOCIDADE = 6
+
+#Cores
 BRANCO = (255,255,255)
 PRETO = (0,0,0)
-def moverJogador1(jogador,tecla,altura_janela):
-    base_superior = 0
-    base_inferior = altura_janela
-    if tecla['cima1'] and jogador['objRect'].top > base_superior:
-        jogador['objRect'].y -= jogador['vel']
-    if tecla['baixo1'] and jogador['objRect'].bottom < base_inferior:
-        jogador ['objRect'].y += jogador['vel']
-def moverJogador2(jogador2,teclas,altura_janela):
-    base_superior = 0
-    base_inferior = altura_janela
-    if teclas['cima2'] and jogador2['objRect'].top > base_superior:
-        jogador2['objRect'].y -= jogador2['vel']
-    if teclas['baixo2'] and jogador2['objRect'].bottom < base_inferior:
-        jogador2['objRect'].y += jogador2['vel']
-def moverBola(bola,dim_janela):
-    base_esquerda = 0
-    base_superior = 0
-    base_direita = dim_janela[0]
-    base_inferior = dim_janela[1]
-    if bola['objRect'].left < base_esquerda or bola['objRect'].right > base_direita:
-        bola['objRect'][0] = LARGURA_JANELA/2
-        bola['objRect'][1] = ALTURA_JANELA/2
-    if bola['objRect'].top < base_superior or bola['objRect'].bottom > base_inferior:
-        bola['velY'] = -bola['velY']
-    bola['objRect'].x -= bola['velX']
-    bola['objRect'].y -= bola['velY']
-bola = {'objRect':pygame.Rect(LARGURA_JANELA/2,ALTURA_JANELA/2,TAMANHO_BOLA,TAMANHO_BOLA),'cor':BRANCO,'velX':VELOCIDADE_BOLAX,'velY':VELOCIDADE_BOLAY}
-teclas = {'cima1':False,'cima2':False,'baixo1':False,'baixo2':False}
-jogador1 = {'objRect':pygame.Rect(20,150,LARGURA_JOGADOR,ALTURA_JOGADOR),'cor':BRANCO,'vel':VELOCIDADE_JOGADOR}
-jogador2 = {'objRect':pygame.Rect(560,150,LARGURA_JOGADOR,ALTURA_JOGADOR),'cor':BRANCO,'vel':VELOCIDADE_JOGADOR}
+
+def pressionartecla(evento,tecla,teclas,nometecla):
+    if evento.key == tecla:
+        teclas[nometecla] = True
+
+def soltartecla(evento,tecla,teclas,nometecla):
+    if evento.key == tecla:
+        teclas[nometecla] = False
+
+class Jogador(pygame.sprite.Sprite):
+
+    def __init__(self,posx):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.Surface([LARGURA_JOGADOR,ALTURA_JOGADOR])
+        self.image.fill(BRANCO)
+        self.rect = self.image.get_rect()
+        self.rect[0] = posx
+        self.rect[1] = ALTURA_JANELA/2 - ALTURA_JOGADOR/2
+        self.vel = VELOCIDADE
+
+    def mover(self,teclas,grupo):
+        if grupo == 1:
+            if teclas['cima'] and self.rect.top > 0:
+                self.rect[1] -= VELOCIDADE
+            if teclas['baixo'] and self.rect.bottom < ALTURA_JANELA:
+                self.rect[1] += VELOCIDADE
+        if grupo == 2:
+            if teclas['cima2'] and self.rect.top > 0:
+                self.rect[1] -= VELOCIDADE
+            if teclas['baixo2'] and self.rect.bottom < ALTURA_JANELA:
+                self.rect[1] += VELOCIDADE
+
+class Bola(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.Surface([TAMANHO_BOLA,TAMANHO_BOLA])
+        self.image.fill(BRANCO)
+        self.rect = self.image.get_rect()
+        self.rect[0] = LARGURA_JANELA/2
+        self.rect[1] = ALTURA_JANELA/2
+        self.velx = VELOCIDADEX
+        self.vely = VELOCIDADEY
+
+    def mover(self,Jogador1,Jogador2):
+        if self.rect.colliderect(Jogador1) and self.rect.left >= 50  or self.rect.colliderect(Jogador2) and self.rect.right <= 650:
+            self.velx = - self.velx
+        elif self.rect.colliderect(Jogador1) and self.rect.left <= 50:
+            self.vely = - self.vely
+        if self.rect.left < 0 or self.rect.right > LARGURA_JANELA:
+            self.rect[0] = LARGURA_JANELA/2
+            self.rect[1] = ALTURA_JANELA/2
+        if self.rect.top < 0 or self.rect.bottom > ALTURA_JANELA:
+            self.vely = - self.vely
+        self.rect[0] += self.velx
+        self.rect[1] += self.vely
 janela = pygame.display.set_mode((LARGURA_JANELA,ALTURA_JANELA))
 pygame.display.set_caption('PyPong')
+
+bola_grupo = pygame.sprite.Group()
+bola = Bola()
+bola_grupo.add(bola)
+
+jogador_grupo = pygame.sprite.Group()
+jogador1 = Jogador(30)
+jogador2 = Jogador(640)
+jogador_grupo.add(jogador1,jogador2)
+
+teclas = {'cima':False,'baixo':False,'cima2':False,'baixo2':False}
+
+cont = 0
+
 relogio = pygame.time.Clock()
+
 deve_continuar = True
+
 while deve_continuar:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             deve_continuar = False
+
         if evento.type == pygame.KEYDOWN:
-            if evento.key == pygame.K_UP:
-                teclas['cima2'] = True
-            if evento.key == pygame.K_DOWN:
-                teclas['baixo2'] = True
-            if evento.key == pygame.K_w:
-                teclas['cima1'] = True
-            if evento.key == pygame.K_s:
-                teclas['baixo1'] = True
+            pressionartecla(evento,pygame.K_w,teclas,'cima')
+            pressionartecla(evento,pygame.K_s,teclas,'baixo')
+            pressionartecla(evento,pygame.K_UP,teclas,'cima2')
+            pressionartecla(evento,pygame.K_DOWN,teclas,'baixo2')
+
         if evento.type == pygame.KEYUP:
-            if evento.key == pygame.K_UP:
-                teclas['cima2'] = False
-            if evento.key == pygame.K_DOWN:
-                teclas['baixo2'] = False
-            if evento.key == pygame.K_w:
-                teclas['cima1'] = False
-            if evento.key == pygame.K_s:
-                teclas['baixo1'] = False
-    bateu = jogador1['objRect'].colliderect(bola['objRect'])
-    bateu2 = jogador2['objRect'].colliderect(bola['objRect'])
-    if bateu or bateu2:
-        bola['velX'] = -bola['velX']
-        bola['velY'] = bola['velY']
-    moverJogador2(jogador2,teclas,ALTURA_JANELA)
-    moverJogador1(jogador1,teclas,ALTURA_JANELA)
-    moverBola(bola,(LARGURA_JANELA,ALTURA_JANELA))
+            soltartecla(evento,pygame.K_w,teclas,'cima')
+            soltartecla(evento,pygame.K_s,teclas,'baixo')
+            soltartecla(evento,pygame.K_UP,teclas,'cima2')
+            soltartecla(evento,pygame.K_DOWN,teclas,'baixo2')
+
+    cont += 1 % 3
+
     janela.fill(PRETO)
-    pygame.draw.rect(janela,jogador2['cor'],jogador2['objRect'])
-    pygame.draw.rect(janela,jogador1['cor'],jogador1['objRect'])
-    pygame.draw.rect(janela,bola['cor'],bola['objRect'])
+
+    jogador1.mover(teclas,1)
+    jogador2.mover(teclas,2)
+    jogador_grupo.update()
+    jogador_grupo.draw(janela)
+
+    bola_grupo.update()
+    bola_grupo.draw(janela)
+    bola.mover(jogador1,jogador2)
+
     pygame.display.update()
+
     relogio.tick(30)
