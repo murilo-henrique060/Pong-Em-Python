065160263d9d395a9eef3fa015c9227e89fb1385
Murilo Henrique <77072462+murilo-henrique060@users.noreply.github.com>
2021-01-18 19:45:48 -0300

    jogo competo
    gg

--- Pong/progama principal.py
+++ Pong/progama principal.py
@@ -1,5 +1,6 @@
-import pygame
-pygame.init()
+from pygame import mixer,init,image,sprite,Surface,display,time,event,QUIT,KEYDOWN,KEYUP,K_w,K_s,K_UP,K_DOWN
+from random import randint
+init()
 
 #diretorio musica
 MUSICA = 'Pong\\PongSoundBlip.wav'
@@ -7,38 +8,47 @@ MUSICA2 = 'Pong\\PongSoundBlip2.wav'
 MUSICA3 = 'Pong\\PongSoundGameOver.wav'
 
 #musica
-COLISAO = pygame.mixer.Sound(MUSICA)
-COLISAO2 = pygame.mixer.Sound(MUSICA2)
-GAMEOVER = pygame.mixer.Sound(MUSICA3)
-
-#Números
-NUM_0 = pygame.image.load('Pong\\numero 0.png')
-NUM_1 = pygame.image.load('Pong\\numero 1.png')
-NUM_2 = pygame.image.load('Pong\\numero 2.png')
-NUM_3 = pygame.image.load('Pong\\numero 3.png')
-NUM_4 = pygame.image.load('Pong\\numero 4.png')
-NUM_5 = pygame.image.load('Pong\\numero 5.png')
-NUM_6 = pygame.image.load('Pong\\numero 6.png')
-NUM_7 = pygame.image.load('Pong\\numero 7.png')
-NUM_8 = pygame.image.load('Pong\\numero 8.png')
-NUM_9 = pygame.image.load('Pong\\numero 9.png')
-
-#Fundo
-FUNDO = pygame.image.load('Pong\\Pong.png')
-
-#Bola
-TAMANHO_BOLA = 30
-VELOCIDADEX = 10
-VELOCIDADEY = 5
+COLISAO = mixer.Sound(MUSICA)
+COLISAO2 = mixer.Sound(MUSICA2)
+GAMEOVER = mixer.Sound(MUSICA3)
 
 #JANELA
 LARGURA_JANELA = 800
 ALTURA_JANELA = 600
 
+
+#Números
+NUM_0 = image.load('Pong\\numero 0.png')
+NUM_1 = image.load('Pong\\numero 1.png')
+NUM_2 = image.load('Pong\\numero 2.png')
+NUM_3 = image.load('Pong\\numero 3.png')
+NUM_4 = image.load('Pong\\numero 4.png')
+NUM_5 = image.load('Pong\\numero 5.png')
+NUM_6 = image.load('Pong\\numero 6.png')
+NUM_7 = image.load('Pong\\numero 7.png')
+NUM_8 = image.load('Pong\\numero 8.png')
+NUM_9 = image.load('Pong\\numero 9.png')
+
+#Fundo
+FUNDO = image.load('Pong\\Pong.png')
+
+#Bola
+TAMANHO_BOLA = 10
+VELOCIDADEx = randint(13,15)
+multiplicadorx = randint(0,1)
+if multiplicadorx == 0:
+    VELOCIDADEx *= -1
+VELOCIDADEy = randint(4,7)
+multiplicador = randint(0,1)
+if multiplicador == 0:
+    VELOCIDADEy *= -1
+POSy = randint(0,ALTURA_JANELA - TAMANHO_BOLA)
+
 #Jogador
-LARGURA_JOGADOR = 30
-ALTURA_JOGADOR = 150
-VELOCIDADE = 8
+LARGURA_JOGADOR = 15
+ALTURA_JOGADOR = 50
+DISTANCIA_DA_BORDA = 50
+VELOCIDADE = 15
 
 #Cores
 BRANCO = (255,255,255)
@@ -74,12 +84,12 @@ def soltartecla(evento,tecla,teclas,nometecla):
     if evento.key == tecla:
         teclas[nometecla] = False
 
-class Jogador(pygame.sprite.Sprite):
+class Jogador(sprite.Sprite):
 
     def __init__(self,posx):
-        pygame.sprite.Sprite.__init__(self)
+        sprite.Sprite.__init__(self)
 
-        self.image = pygame.Surface([LARGURA_JOGADOR,ALTURA_JOGADOR])
+        self.image = Surface([LARGURA_JOGADOR,ALTURA_JOGADOR])
         self.image.fill(BRANCO)
         self.rect = self.image.get_rect()
         self.rect[0] = posx
@@ -98,37 +108,39 @@ class Jogador(pygame.sprite.Sprite):
             if teclas['baixo2'] and self.rect.bottom < ALTURA_JANELA:
                 self.rect[1] += VELOCIDADE
 
-class Bola(pygame.sprite.Sprite):
+class Bola(sprite.Sprite):
 
     def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
+        sprite.Sprite.__init__(self)
 
-        self.image = pygame.Surface([TAMANHO_BOLA,TAMANHO_BOLA])
+        self.image = Surface([TAMANHO_BOLA,TAMANHO_BOLA])
         self.image.fill(BRANCO)
         self.rect = self.image.get_rect()
         self.rect[0] = LARGURA_JANELA/2
-        self.rect[1] = ALTURA_JANELA/2
-        self.velx = VELOCIDADEX
-        self.vely = VELOCIDADEY
+        self.rect[1] = POSy
+        self.velx = VELOCIDADEx
+        self.vely = VELOCIDADEy
         self.cont = 0
         self.cont2 = 0
 
-    def mover(self,Jogador1,Jogador2):
+    def mover(self,Jogador1,Jogador2,VELOCIDADEX):
+        if VELOCIDADEX < 0:
+            VELOCIDADEX *= -1
         if self.rect.colliderect(Jogador1) or self.rect.colliderect(Jogador2):
             if self.cont2 == 0:
-                pygame.mixer.Sound.play(COLISAO)
+                mixer.Sound.play(COLISAO)
             self.cont2 += 1
         else:
             self.cont2 = 0
-        if self.rect.colliderect(Jogador1) and self.rect.left >= 50 and self.cont == 0 or self.rect.colliderect(Jogador2) and self.rect.right <= 750 and self.cont == 0:
+        if self.rect.colliderect(Jogador1) and self.rect.left >= Jogador1.rect.right - VELOCIDADEX and self.cont == 0 or self.rect.colliderect(Jogador2) and self.rect.right <= Jogador2.rect.left + VELOCIDADEX and self.cont == 0:
             self.velx = - self.velx
-        elif self.rect.colliderect(Jogador1) and self.rect.left < 50 or self.rect.colliderect(Jogador2) and self.rect.right > 750:
-            if self.rect.colliderect(Jogador1) and self.rect[1] + 30 <= Jogador1.rect[1] + 70 or self.rect.colliderect(Jogador2) and self.rect[1] + 30 <= Jogador2.rect[1] + 70:
+        elif self.rect.colliderect(Jogador1) and self.rect.left < Jogador1.rect.right - VELOCIDADEX or self.rect.colliderect(Jogador2) and self.rect.right > Jogador2.rect.left + VELOCIDADEX:
+            if self.rect.colliderect(Jogador1) and self.rect[1] + TAMANHO_BOLA <= Jogador1.rect[1] + ALTURA_JOGADOR / 2  or self.rect.colliderect(Jogador2) and self.rect[1] + TAMANHO_BOLA <= Jogador2.rect[1] + ALTURA_JOGADOR:
                 if self.vely < 0:
                     self.vely *= 1.3
                 elif self.vely > 0:
                     self.vely *= -1
-            elif self.rect.colliderect(Jogador1) and self.rect[1] >= Jogador1.rect[1] + 80 or self.rect.colliderect(Jogador2) and self.rect[1] >= Jogador2.rect[1] + 80:
+            elif self.rect.colliderect(Jogador1) and self.rect[1] >= Jogador1.rect[1] + ALTURA_JOGADOR / 2 + 1 or self.rect.colliderect(Jogador2) and self.rect[1] >= Jogador2.rect[1] + TAMANHO_BOLA:
                 if self.vely < 0:
                     self.vely *= -1
                 elif self.vely > 0:
@@ -138,26 +150,35 @@ class Bola(pygame.sprite.Sprite):
         else:
             self.cont = 0
         if self.rect.left < 0 or self.rect.right > LARGURA_JANELA:
-            pygame.mixer.Sound.play(GAMEOVER)
-            self.vely = VELOCIDADEY
-            self.velx = - self.velx
+            mixer.Sound.play(GAMEOVER)
+            POSy = randint(0,ALTURA_JANELA - TAMANHO_BOLA)
+            VELOCIDADEx = randint(13,15)
+            multiplicadorx = randint(0,1)
+            if multiplicadorx == 0:
+                VELOCIDADEx *= -1
+            VELOCIDADEy = randint(5,7)
+            multiplicador = (0,1)
+            if multiplicador == 0:
+                VELOCIDADEy *= -1
+            self.vely = VELOCIDADEy
+            self.velx = VELOCIDADEx
             self.rect[0] = LARGURA_JANELA/2
-            self.rect[1] = ALTURA_JANELA/2
+            self.rect[1] = POSy
         if self.rect.top < 0 or self.rect.bottom > ALTURA_JANELA:
-            pygame.mixer.Sound.play(COLISAO2)
+            mixer.Sound.play(COLISAO2)
             self.vely = - self.vely
         self.rect[0] += self.velx
         self.rect[1] += self.vely
-janela = pygame.display.set_mode((LARGURA_JANELA,ALTURA_JANELA))
-pygame.display.set_caption('PyPong')
+janela = display.set_mode((LARGURA_JANELA,ALTURA_JANELA))
+display.set_caption('PyPong')
 
-bola_grupo = pygame.sprite.Group()
+bola_grupo = sprite.Group()
 bola = Bola()
 bola_grupo.add(bola)
 
-jogador_grupo = pygame.sprite.Group()
-jogador1 = Jogador(30)
-jogador2 = Jogador(740)
+jogador_grupo = sprite.Group()
+jogador1 = Jogador(DISTANCIA_DA_BORDA)
+jogador2 = Jogador(LARGURA_JANELA - (DISTANCIA_DA_BORDA + LARGURA_JOGADOR))
 jogador_grupo.add(jogador1,jogador2)
 
 teclas = {'cima':False,'baixo':False,'cima2':False,'baixo2':False}
@@ -165,38 +186,29 @@ teclas = {'cima':False,'baixo':False,'cima2':False,'baixo2':False}
 jogador1_pontos = 0
 jogador2_pontos = 0
 
-relogio = pygame.time.Clock()
+relogio = time.Clock()
 
 cont = 0
 
 deve_continuar = True
-deve_iniciar = True
 
 while deve_continuar:
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
+    for evento in event.get():
+        if evento.type == QUIT:
             deve_continuar = False
 
-        if evento.type == pygame.KEYDOWN:
+        if evento.type == KEYDOWN:
             deve_iniciar = False
-            pressionartecla(evento,pygame.K_w,teclas,'cima')
-            pressionartecla(evento,pygame.K_s,teclas,'baixo')
-            pressionartecla(evento,pygame.K_UP,teclas,'cima2')
-            pressionartecla(evento,pygame.K_DOWN,teclas,'baixo2')
+            pressionartecla(evento,K_w,teclas,'cima')
+            pressionartecla(evento,K_s,teclas,'baixo')
+            pressionartecla(evento,K_UP,teclas,'cima2')
+            pressionartecla(evento,K_DOWN,teclas,'baixo2')
 
-        if evento.type == pygame.KEYUP:
-            soltartecla(evento,pygame.K_w,teclas,'cima')
-            soltartecla(evento,pygame.K_s,teclas,'baixo')
-            soltartecla(evento,pygame.K_UP,teclas,'cima2')
-            soltartecla(evento,pygame.K_DOWN,teclas,'baixo2')
-
-    if deve_iniciar:
-        bola.velx = 0
-        bola.vely = 0
-    elif cont == 0:
-        bola.velx = VELOCIDADEX
-        bola.vely = VELOCIDADEY
-        cont += 1
+        if evento.type == KEYUP:
+            soltartecla(evento,K_w,teclas,'cima')
+            soltartecla(evento,K_s,teclas,'baixo')
+            soltartecla(evento,K_UP,teclas,'cima2')
+            soltartecla(evento,K_DOWN,teclas,'baixo2')
 
     if bola.rect.left < 0:
         jogador2_pontos += 1
@@ -212,7 +224,7 @@ while deve_continuar:
     janela.blit(FUNDO, (0,0))
 
     desenhar_ponto(janela,jogador1_pontos,(150,50))
-    desenhar_ponto(janela, jogador2_pontos,(525,50))
+    desenhar_ponto(janela, jogador2_pontos,(550,50))
 
     jogador1.mover(teclas,1)
     jogador2.mover(teclas,2)
@@ -221,8 +233,8 @@ while deve_continuar:
 
     bola_grupo.update()
     bola_grupo.draw(janela)
-    bola.mover(jogador1,jogador2)
+    bola.mover(jogador1,jogador2,VELOCIDADEx)
 
-    pygame.display.update()
+    display.update()
 
-    relogio.tick(40)
+    relogio.tick(30)
